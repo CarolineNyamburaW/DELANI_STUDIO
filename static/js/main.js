4684c946b56f0625922ced55eb10860853649839
CarolineNyamburaW <caroline.nyambura@student.moringaschool.com>
2021-06-21 13:19:03 +0300

    added a function for my first clickable element

--- static/js/main.js
+++ static/js/main.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
   $("img").click(function() {
     $("p").show();
   });
+  
+});$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $("#design-showing").toggle();
+    $("#design-hidden").toggle();
+  });
 });
