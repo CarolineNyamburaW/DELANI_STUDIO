eda5a1d4db55473e54bcda4cb6d1485be06d57c7
CarolineNyamburaW <caroline.nyambura@student.moringaschool.com>
2021-06-21 13:19:54 +0300

    added a function for my second clickable element

--- static/js/main.js
+++ static/js/main.js
@@ -2,10 +2,17 @@ $(document).ready(function() {
   $("img").click(function() {
     $("p").show();
   });
-  
+
 });$(document).ready(function() {
   $(".clickable1").click(function() {
     $("#design-showing").toggle();
     $("#design-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+    $("#development-showing").toggle();
+    $("#development-hidden").toggle();
   });
 });
